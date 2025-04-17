17a144b16b5cadd4c7e365d4e77f27deabb55dc7
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-04-17 09:44:39 +0530

    16-04-2025 DSA.cpp

--- DSA/16-04-2025 DSA.cpp
+++ DSA/16-04-2025 DSA.cpp
@@ -1,1 +1,31 @@
-// Ques - 
+/* 
+Ques - https://neetcode.io/problems/is-anagram
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        for (char& c : s) {
+            for (char& e : t) {
+                if (c == e) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+*/
