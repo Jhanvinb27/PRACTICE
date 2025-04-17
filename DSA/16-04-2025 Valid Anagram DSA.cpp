d8a0a23d06412f17bfbeefccdfd184e1e0f75cb9
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-04-17 12:27:51 +0530

    16-04-2025 Valid Anagram DSA.cpp

--- DSA/16-04-2025 Valid Anagram DSA.cpp
+++ DSA/16-04-2025 Valid Anagram DSA.cpp
@@ -1,6 +1,27 @@
 /* 
 Ques - https://neetcode.io/problems/is-anagram
 
+Valid Anagram
+
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+An anagram is a string that contains the exact same characters as another string, 
+but the order of the characters can be different.
+
+Example 1:
+Input: s = "racecar", t = "carrace"
+Output: true
+
+Example 2:
+Input: s = "jar", t = "jam"
+Output: false
+Constraints:
+
+s and t consist of lowercase English letters.
+*/
+
+/*
+Approach 
+*/
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -8,9 +29,7 @@ public:
         int m = t.length();
         for (char& c : s) {
             for (char& e : t) {
-                if (c == e) {
-                    return true;
-                }
+                return s== t;
             }
         }
         return false;
@@ -28,4 +47,3 @@ public:
     }
 };
 
-*/
