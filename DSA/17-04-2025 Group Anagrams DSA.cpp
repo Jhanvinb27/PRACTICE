0b826fec04c2ff4a57323a004b889250d3d466df
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-04-18 19:24:15 +0530

    17-04-2025 Group Anagrams DSA.cpp

--- DSA/17-04-2025 Group Anagrams DSA.cpp
+++ DSA/17-04-2025 Group Anagrams DSA.cpp
@@ -32,9 +32,41 @@ Approach -
 */
 
 /*
-Solution 1 - we can treverse through each word and store it in a hashmap
+Solution 1 - we can sort each word
+           - and if the same word is repeated the next 1 time [i+1], or two times [i+2] or more ...
+           - we'll make a sublist and store it there 
+           - similarly we can store other words as well
+           - next we can store all the sub list in the final result array and can return it.
+- Let's Code it:
+*/
+# include <bits/stdc++.h>
+using namespace std;
+  vector <vector<string>> GroupedAnagrams (vector<string>& strs){
+    sort(strs.begin(), strs.end());
+    vector <string> sublists;
+    for (string s : strs){
+      if (s==s+1){
+        sublists.push_backstrs[s];
+      }
+   }
+    return sublists;
+}
+int main() {
+  strs = ["act","pots","tops","cat","stop","hat"]
+  vector<string> result = GroupedAnagrams;
+}
 
 
+/*
+OPTIMAL 
+    - we will extract each lettter from each word of the array
+    - then we will use positions from 0-25 number for a to z : for each and every single word of the array
+    - this is to keep the track of appearance of each word.
+    - then we will group a to z based on their number count of letters
+    i.e if "cat" -> a=1,b=0,c=1,d=0,...,t=1,u=0,v=0,...,z=0
+    and for some othe word "pot" -> a=0,b=0,...,o=1,p=1,q=0,r=0,...,t=1,u=0,v=0,...,z=0
+    - so judging by the count of each letter and it's frequency we will group them together
+*/
 
 
 
