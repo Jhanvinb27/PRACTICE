1eb2d0d1c802483c9c9b2d61b7ec044208d1d9a1
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-04-21 21:16:15 +0530

    21-04-2025 3Sum DSA.cpp

---
+++ DSA/21-04-2025 3Sum DSA.cpp
@@ -0,0 +1,81 @@
+/*
+Ques - https://neetcode.io/problems/three-integer-sum
+*/
+/*
+Given an integer array nums, return all the triplets 
+[nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and 
+the indices i, j and k are all distinct.
+The output should not contain any duplicate triplets. 
+You may return the output and the triplets in any order.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+Constraints:
+3 <= nums.length <= 1000
+-10^5 <= nums[i] <= 10^5
+*/
+/*
+Approach - sorting the array to make it easier to skip duplicates
+         - for loop + two-pointer approach to find valid triplets 
+         - O(n²) time
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(n, arr);
+    return 0;
+}
+
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
