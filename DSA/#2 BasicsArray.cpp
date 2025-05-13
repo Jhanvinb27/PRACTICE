0b903f9f2d1674c75165221954a6af79c509f07b
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-05-13 10:38:09 +0530

    #2 BasicsArray.cpp

--- DSA/#2 BasicsArray.cpp
+++ DSA/#2 BasicsArray.cpp
@@ -1,1 +1,21 @@
+Ques - Print the array
 
+# include <bits/stdc++.h>
+using namespace std;
+
+void Print (vector<int> array){
+  cout << "{";
+  int n = array.size();
+  for (int i = 0; i < n; i++) {
+    cout << array[i];
+    if( i < n - 1){
+      cout << ",";
+    }
+  }
+  cout << "}";
+}
+int main(){
+  vector<int> array = {2,3,4,5};
+  Print(array);
+  return 0;
+}
