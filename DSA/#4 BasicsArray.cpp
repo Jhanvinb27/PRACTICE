94bcae3f42f43bc33cc544b6ca0c52b61ae581a1
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-05-13 23:23:00 +0530

    #4 BasicsArray.cpp

--- DSA/#4 BasicsArray.cpp
+++ DSA/#4 BasicsArray.cpp
@@ -19,3 +19,29 @@ int main() {
   cout << Max(array) << endl;
   return 0;
 }
+
+# include <bits/stdc++.h>
+using namespace std;
+
+int Max2(const vector<int>& array) {
+    int n = array.size();
+    int max1 = max(array[0], array[1]);
+    int max2 = min(array[0], array[1]);
+    for (int i = 2; i < n; i++) {
+        int x = array[i];
+        if (x > max1) {
+            max2 = max1;
+            max1 = x;
+        }
+        else if (x > max2) {
+            max2 = x;
+        }
+    }
+    return max2;
+}
+
+int main (){
+  vector<int> array = {2,3,4,5,6};
+  cout << Max2(array) << endl;
+  return 0;
+}
