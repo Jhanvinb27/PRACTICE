1a7463182ab79d71834e45c9e96d38fa454cf0e6
Jhanvinb27 <133041900+Jhanvinb27@users.noreply.github.com>
2025-05-14 16:48:33 +0530

    14-04-2025 Two Sum DSA.cpp

--- DSA/Sums/14-04-2025 Two Sum DSA.cpp
+++ DSA/Sums/14-04-2025 Two Sum DSA.cpp
@@ -4,7 +4,8 @@ Ques - https://neetcode.io/problems/two-integer-sum
 
 Two Sum
 
-Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+Given an array of integers nums and an integer target, 
+return the indices i and j such that nums[i] + nums[j] == target and i != j.
 You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 Return the answer with the smaller index first.
 
